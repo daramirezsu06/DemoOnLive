0bb641e6175d9ff87f2610dc6aaac15276c4213f
daramirezsu06 <daramirezsu06@gmail.com>
2024-04-10 18:16:46 -0500

    modificacion ahora tomamos los personajes directamente desde la API

--- index.js
+++ index.js
@@ -1,91 +1,12 @@
-let tempData = [
-  {
-    id: 1,
-    name: "Rick Sanchez",
-    status: "Alive",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-  },
-  {
-    id: 2,
-    name: "Morty Smith",
-    status: "Alive",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
-  },
-  {
-    id: 3,
-    name: "Summer Smith",
-    status: "Alive",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
-  },
-  {
-    id: 4,
-    name: "Beth Smith",
-    status: "Alive",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
-  },
-  {
-    id: 5,
-    name: "Jerry Smith",
-    status: "Alive",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
-  },
-  {
-    id: 6,
-    name: "Abadango Cluster Princess",
-    status: "Alive",
-    species: "Alien",
-    image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
-  },
-  {
-    id: 7,
-    name: "Abradolf Lincler",
-    status: "unknown",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/7.jpeg",
-  },
-  {
-    id: 8,
-    name: "Adjudicator Rick",
-    status: "Dead",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/8.jpeg",
-  },
-  {
-    id: 9,
-    name: "Agency Director",
-    status: "Dead",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/9.jpeg",
-  },
-  {
-    id: 10,
-    name: "Alan Rails",
-    status: "Dead",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/10.jpeg",
-  },
-  {
-    id: 11,
-    name: "Albert Einstein",
-    status: "Dead",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/11.jpeg",
-  },
-  {
-    id: 12,
-    name: "Alexander",
-    status: "Dead",
-    species: "Human",
-    image: "https://rickandmortyapi.com/api/character/avatar/12.jpeg",
-  },
-];
+let tempData = [];
+
+$.get("https://rickandmortyapi.com/api/character", (data, status) => {
+  tempData = data.results;
+  console.log(tempData);
+});
 
 const root = document.getElementById("root");
+const getCharacters = document.getElementById("get-characters");
 
 const deleteCharacter = (event) => {
   event.preventDefault();
@@ -128,4 +49,6 @@ const createCart = (arrayCharacters = []) => {
   });
 };
 
-createCart(tempData);
+getCharacters.addEventListener("click", () => {
+  createCart(tempData);
+});
